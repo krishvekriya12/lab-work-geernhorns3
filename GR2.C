475977cface2097551b2666eb8eacd75989972c7
krish vekriya <131762007+krishvekriya12@users.noreply.github.com>
2023-04-28 15:47:32 +0530

    Add files via upload

---
+++ GR2.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	clrscr();
+	int k,r,i;
+	printf("enter value k=");
+	scanf("%d",&r);
+	printf("enter value r=");
+	scanf("%d",&k);
+	printf("enter value i=");
+	scanf("%d",&i);
+	printf("additon of k=%d",k);
+	printf("additon of r=%d",r);
+	getch();
+}
+
+
+
+
+
